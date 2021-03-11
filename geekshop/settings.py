5f55ce8388914f0a8e853a32ed1e8051d9958396
Alex-Ruf <68159105+Alex-Ruf@users.noreply.github.com>
2021-03-11 19:45:51 +0300

    Task 4

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'mainapp',
+    'authapp',
+
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -129,3 +131,5 @@ STATICFILES_DIRS = (
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT=os.path.join (BASE_DIR , 'media')
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
